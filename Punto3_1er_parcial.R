e986f098cf6f40846fb57a6b3af94feb8f12ffe6
Lucesita <luzpaceuni@gmail.com>
2025-05-23 21:22:05 -0300

    Create Punto3_1er_parcial.R

---
+++ Punto3_1er_parcial.R
@@ -0,0 +1,31 @@
+#n = número de ensayos
+#p = probabilidad de éxito en cada ensayo
+#x = número de éxitos deseados
+
+
+n_ensayos <- 5  #es ejemplo hay que cambiarlo antes de iniciar
+p_exito <- 0.5  #es ejemplo hay que cambiarlo antes de iniciar
+x <- 2          #es ejemplo hay que cambiarlo antes de iniciar
+
+
+# probabilidad puntual P(X = x)
+bi_puntual <- function(n_ensayos, p_exito, x){
+    if (is.null(x)) stop("Debe ingresar el número de éxitos x.")
+    return(dbinom(x, size = n_ensayos, prob = p_exito))
+}
+# probabilidad acumulada P(X ≤ x)
+bi_acum <- function(n_ensayos, p_exito, x){
+    if (is.null(x)) stop("Debe ingresar el valor máximo x.")
+    return(pbinom(x, size = n_ensayos, prob = p_exito))
+}
+# probabilidad aumulada complementaria P(X > x)
+bi_mayor <- function(n_ensayos, p_exito, x){
+    if (is.null(x)) stop("Debe ingresar el valor máximo x.")
+    if (x < 0 || x > n) stop("x debe estar entre 0 y n.")
+    return(1 - pbinom(x, size = n, prob = p))}
+
+esperanza <- n * p
+varianza <- n * p * (1 - p)
+
+print(paste("Esperanza:", esperanza))
+print(paste("Varianza:", varianza))
